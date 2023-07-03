93cf2c764dc22ce56d3aa5546ce41646aad16567
piyushkumar12111111 <103259442+piyushkumar12111111@users.noreply.github.com>
2023-07-03 23:40:00 +0530

    first commit

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Piyush Kumar, a Flutter developer, Technical blog writer and UI designer.';
+
+  static const String aboutMeDetail =
+      'I\'m a Third Year B.Tech Student  with ECE as Majors from Army Institute of Technology, Pune. I have been developing mobile apps for over 6 months now. I have worked in teams for some startups and helped them in launching their prototypes and got valuable learning experience. Been a member of Google Developer Student Clubs (DSC). Currently working as intern at Echio ';
+}
